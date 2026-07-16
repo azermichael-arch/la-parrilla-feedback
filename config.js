aea31c39e65094a0098d2a753cad1ecfbcf23f4e
azermichael-arch <azer.michael@gmail.com>
2026-07-16 19:47:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,50 @@
 const CONFIG = {
 
+    restaurant: "La Parrilla",
+
     branch: "Life Sports Club",
 
     device: "Tablet-01",
 
-    apiUrl: "https://script.google.com/macros/s/AKfycbxD2q9lNsvNlxBoIsT63lMUsd5JGG1SbjgqhuX1ckCiJPGFy6M2SoKxiGp79UyKXDNU/exec"
+    apiUrl: "https://script.google.com/macros/s/AKfycbxD2q9lNsvNlxBoIsT63lMUsd5JGG1SbjgqhuX1ckCiJPGFy6M2SoKxiGp79UyKXDNU/exec",
+
+    version: "2.1",
+
+    mode: "feedback",
+
+    promotionTime: 8000,
+
+    thankYouTime: 2000,
+
+    adminPassword: "1234"
 
 };
+
+
+const PROMOTIONS = [
+
+    {
+        image: "images/burger1.jpg",
+        title: "DOUBLE CHEESE BURGER",
+        price: "220 EGP"
+    },
+
+    {
+        image: "images/familybox.jpg",
+        title: "FAMILY FAN BOX",
+        price: "880 EGP"
+    },
+
+    {
+        image: "images/icecream.jpg",
+        title: "SOFT ICE CREAM",
+        price: "55 EGP"
+    },
+
+    {
+        image: "images/chicken.jpg",
+        title: "CHICKEN STRIPS",
+        price: "190 EGP"
+    }
+
+];
